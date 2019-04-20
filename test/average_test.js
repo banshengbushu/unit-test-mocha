aec5e760324b0c0968319415c8dcd75980d23a60
banshengbushu <wangdanna1995@outlook.com>
2019-04-20 16:01:47 +0800

    Add all test cases

--- test/average_test.js
+++ test/average_test.js
@@ -1,19 +1,20 @@
 const should = require('should');
 const average_test = require('../script/average');
 
-describe('#average_test', () => {
-    it('should return the average_test of array', done => {
-        const input = [[1, 2, 3, 4]];
-        const expectResult = 2.5;
-        const result = average_test(input);
+    describe('#average_test', () => {
+        it('should return the average_test of array', done => {
+            const input = [[1, 2, 3, 4]];
+            const expectResult = 2.5;
+            const result = average_test(input);
 
-        result.should.equal(expectResult);
+            result.should.equal(expectResult);
+        });
+
+        it('should return NaN when array is empty', done => {
+            const input = [[]];
+            const result = average_test(input);
+
+            isNaN(result).should.be.true;
+        });
     });
 
-    it('should return NaN when array is empty', done => {
-        const input = [[]];
-        const result = average_test(input);
-
-        isNaN(result).should.be.true;
-    });
-});
--- test/max_test.js
+++ test/max_test.js
@@ -1,18 +1,18 @@
 const should = require('should')
 const max = require('../script/max')
 
-describe('#max_test', () => {
-    it('should return the maximum in array', done => {
-        const input = [1, 10, 100, 1000];
-        const result = max(input);
+    describe('#max_test', () => {
+        it('should return the maximum in array', done => {
+            const input = [1, 10, 100, 1000];
+            const result = max(input);
 
-        result.should.equal(1000);
+            result.should.equal(1000);
+        });
+
+        it('should return a string with input is an empty array', done => {
+            const input = [];
+            const result = max(input);
+
+            result.should.equal('Input is an empty array');
+        });
     });
-
-    it('should return undefined when array is empty', done => {
-        const input = [];
-        const result = max(input);
-
-        (result === undefined).should.be.true;
-    });
-});
