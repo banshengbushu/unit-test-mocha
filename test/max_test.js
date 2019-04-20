6897d9da6b061cc7da838ad33d449ef743747c58
banshengbushu <wangdanna1995@outlook.com>
2019-04-20 16:25:16 +0800

    Remove max test file

--- test/max_test.js
+++ test/max_test.js
@@ -2,19 +2,5 @@ const should = require('should')
 const max = require('../script/max')
 
 describe('#max_test', () => {
-    it('should return the maximum in array', done => {
-        const input = [1, 10, 100, 1000];
-        const result = max(input);
 
-        result.should.equal(1000);
-        done();
-    });
-
-    it('should return a string with input is an empty array', done => {
-        const input = [];
-        const result = max(input);
-
-        result.should.equal('Input is an empty array');
-        done();
-    });
 });
